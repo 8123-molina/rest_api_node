17b444b22fee9a31d74d8f46994a103c37b5cc0d
Dhyone <molinaprojetos@gmail.com>
2022-07-16 15:00:53 -0300

    Instalando morgan para monitorar

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const rotaCard = require('./routes/cards');
 const rotaCategoria = require('./routes/categorias');
+
+app.use(morgan('dev'));
 
 app.use('/card', rotaCard);
 app.use('/categoria', rotaCategoria);
